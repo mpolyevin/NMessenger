75b423b4e81d883744ed44c24bbf3fef47cab215
Alex Tau <tau.alexandru@gmail.com>
2018-05-17 15:01:08 +0300

    cameraButton

--- nMessenger/Source/Messenger/Components/InputBarView/NMessengerBarView.swift
+++ nMessenger/Source/Messenger/Components/InputBarView/NMessengerBarView.swift
@@ -21,6 +21,7 @@ open class NMessengerBarView: InputBarView, UITextViewDelegate, CameraViewDelega
     
     //MARK: IBOutlets
     //@IBOutlet for InputBarView
+    @IBOutlet open var cameraButton: UIButton!
     @IBOutlet open weak var inputBarView: UIView!
     //@IBOutlet for send button
     @IBOutlet open weak var sendButton: UIButton!
